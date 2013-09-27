f9b7d3b4003ca8776a2cae29eb7c6249af7c2d1b
Max Goldstein <maxgoldstein1@gmail.com>
2013-09-26 22:42:10 -0400

    Color fades now specified to go both ways

--- libFade.elm
+++ libFade.elm
@@ -24,30 +24,26 @@ the input signal changes -}
 fade : Time -> Signal a -> Signal Float
 fade dt trig = let
     plusReset t acc = if t == 0 then 0 else acc + t
-    clock = foldp plusReset 0 (fpsWhen 50 (since dt trig))
-               in (\t -> inMilliseconds t / inMilliseconds dt) <~ clock
+    active = dt `since` trig
+    clock = foldp plusReset 0 (fpsWhen 50 active)
+       in (\t -> inMilliseconds t / inMilliseconds dt) <~ clock
 
 -- Todo: go both ways (require input to be Signal Bool?)
 {-| Fade between Colors over a Time when the input signal changes. -}
-fadeColor : Color -> Color -> Time -> Signal a -> Signal Color
-fadeColor c1 c2 dt trig = interpolateColor <~ constant c1 ~ constant c2 ~ fade dt trig
+fadeColor : Color -> Color -> Time -> Signal Bool -> Signal Color
+fadeColor c1 c2 dt trig = let
+    trues  = interpolateColor <~ constant c1 ~ constant c2 ~ fade dt (keepIf id True trig)
+    falses = interpolateColor <~ constant c2 ~ constant c1 ~ fade dt (keepIf not False trig)
+        in merge trues falses
 
 -- Todo: fade between rects and circles
 
 -- Testing code
+--main = asText <~ fade (0.8*second) (fps 0.5)
+
 bg (w,h) clr = collage w h [rect (toFloat w) (toFloat h) |> filled clr]
-main = bg <~ Window.dimensions ~ fadeColor blue red second (fps 0.5)
-
-
-
-{-- Co-recursive version that causes runtime error
-fade : Time -> Signal a -> Signal Float
-fade dt trig = let
-    plusReset t acc = if t == 0 then 0 else acc + t
-    clock = foldp plusReset 0 (fpsWhen 30 active)
-    start = sampleOn trig <| constant True
-    end = ((>=) dt) <~ clock |> dropIf id False
-    active = merge start end
-               in (\t -> inMilliseconds t / inMilliseconds dt) <~ clock
---}
+main = let trues = (\_->True) <~ fps 0.5
+           falses = (\_->False) <~ delay second trues
+           trig = merge trues falses
+       in bg <~ Window.dimensions ~ fadeColor blue red second trig
 
